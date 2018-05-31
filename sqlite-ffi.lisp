b797492b17a279de4206517548d60da6ce0c3f75
Madhu <enometh@net.meer>
2018-05-31 18:37:51 +0530

    sqlite-ffi.lisp: support user functions

--- sqlite-ffi.lisp
+++ sqlite-ffi.lisp
@@ -30,7 +30,53 @@
            :sqlite3-bind-blob
            :destructor-transient
            :destructor-static
-           :sqlite3-last-insert-rowid))
+           :sqlite3-last-insert-rowid
+
+   :sqlite3-context
+   :sqlite3-value
+
+   :+sqlite-utf8+
+   :+sqlite-utf16le+
+   :+sqlite-utf16be+
+   :+sqlite-utf16+
+   :+sqlite-any+
+   :+sqlite-utf16-aligned+
+   :+sqlite-static+
+   :+sqlite-transient+
+
+   :sqlite3-result-blob
+   :sqlite3-result-double
+   :sqlite3-result-error
+   :sqlite3-result-error16
+   :sqlite3-result-error16
+   :sqlite3-result-error-toobig
+   :sqlite3-result-error-nomem
+   :sqlite3-result-error-code
+   :sqlite3-result-int
+   :sqlite3-result-int64
+   :sqlite3-result-null
+   :sqlite3-result-text
+   :sqlite3-result-text16
+   :sqlite3-result-text16le
+   :sqlite3-result-text16be
+   :sqlite3-result-value
+   :sqlite3-result-zeroblob
+
+   :sqlite3-value-zeroblob
+   :sqlite3-value-bytes
+   :sqlite3-value-bytes16
+   :sqlite3-value-double
+   :sqlite3-value-int
+   :sqlite3-value-int64
+   :sqlite3-value-text
+   :sqlite3-value-text16
+   :sqlite3-value-text16le
+   :sqlite3-value-text16be
+   :sqlite3-value-type
+   :sqlite3-value-numeric-type
+
+   :sqlite3-create-function
+))
 
 (in-package :sqlite-ffi)
 
@@ -198,3 +244,205 @@
 
 (defcfun sqlite3-last-insert-rowid :int64
   (db p-sqlite3))
+
+
+;;; ----------------------------------------------------------------------
+;;;
+;;; ;madhu 180407
+;;;
+;; text encodings supported by SQLite.
+(defvar +SQLITE-UTF8+ 1)
+(defvar +SQLITE-UTF16LE+ 2)
+(defvar +SQLITE-UTF16BE+ 3)
+(defvar +SQLITE-UTF16+ 4)	       ; native byte order
+(defvar +SQLITE-ANY+ 5)
+(defvar +SQLITE-UTF16-ALIGNED 8)       ; sqlite3_create_collation only
+
+(defcstruct sqlite3-context)
+(defcstruct sqlite3-value)
+
+(defctype p-sqlite3-context (:pointer (:struct sqlite3-context)))
+(defctype p-sqlite3-value (:pointer (:struct sqlite3-value)))
+(defctype p-p-sqlite3-value (:pointer (:pointer (:struct sqlite3-value))))
+
+(defvar +SQLITE-STATIC+ (sqlite-ffi:destructor-static))
+(defvar +SQLITE-TRANSIENT+ (sqlite-ffi:destructor-transient))
+
+;; typedef void (*sqlite3_destructor_type)(void*);
+;; void sqlite3_result_blob(sqlite3_context*, const void*, int, void(*)(void*));
+
+;; FIXME: this is autoconverted by cffi with 0-terminated strings only!
+
+;; If the 3rd parameter to the sqlite3_result_text* interfaces is
+;; negative, then SQLite takes result text from the 2nd parameter
+;; through the first zero character. If the 3rd parameter to the
+;; sqlite3_result_text* interfaces is non-negative, then as many bytes
+;; (not characters) of the text pointed to by the 2nd parameter are
+;; taken as the application-defined function result.
+
+;;  If the 4th parameter to the sqlite3_result_text* interfaces or
+;;  sqlite3_result_blob is a non-NULL pointer, then SQLite calls that
+;;  function as the destructor on the text or BLOB result when it has
+;;  finished using that result. If the 4th parameter to the
+;;  sqlite3_result_text* interfaces or sqlite3_result_blob is the
+;;  special constant SQLITE_STATIC, then SQLite assumes that the text
+;;  or BLOB result is in constant space and does not copy the it or
+;;  call a destructor when it has finished using that result. If the
+;;  4th parameter to the sqlite3_result_text* interfaces or
+;;  sqlite3_result_blob is the special constant SQLITE_TRANSIENT then
+;;  SQLite makes a copy of the result into space obtained from from
+;;  sqlite3_malloc() before it returns.
+
+(defcfun sqlite3-result-blob :pointer
+  (ctx p-sqlite3-context)
+  (buf :pointer)
+  (n :int)
+  (func :pointer))
+
+(defcfun sqlite3-result-double :void
+  (ctx p-sqlite3-context)
+  (val :double))
+
+(defcfun sqlite3-result-error :void
+  (ctx p-sqlite3-context)
+  (msg :string)
+  (len :int))
+
+(defcfun sqlite3-result-error16 :void
+  (ctx p-sqlite3-context)
+  (msg :string)
+  (len :int))
+
+(defcfun sqlite3-result-error-toobig :void
+  (ctx p-sqlite3-context))
+
+(defcfun sqlite3-result-error-nomem :void
+  (ctx p-sqlite3-context))
+
+(defcfun sqlite3-result-error-code :void
+  (ctx p-sqlite3-context)
+  (code :int))
+
+(defcfun sqlite3-result-int :void
+  (ctx p-sqlite3-context)
+  (val :int))
+
+(defcfun sqlite3-result-int64 :void
+  (ctx p-sqlite3-context)
+  (val :int64))
+
+(defcfun sqlite3-result-null :void
+  (ctx p-sqlite3-context))
+
+(defcfun sqlite3-result-text :void
+  (ctx p-sqlite3-context)
+  (str :string)
+  (len :int)
+  (destructor :pointer))
+
+(defcfun sqlite3-result-text16 :void
+  (ctx p-sqlite3-context)
+  (str :string)
+  (len :int)
+  (destructor :pointer))
+
+(defcfun sqlite3-result-text16le :void
+  (ctx p-sqlite3-context)
+  (str :string)
+  (len :int)
+  (destructor :pointer))
+
+(defcfun sqlite3-result-text16be :void
+  (ctx p-sqlite3-context)
+  (str :string)
+  (len :int)
+  (destructor :pointer))
+
+(defcfun sqlite3-result-value :void
+  (ctx p-sqlite3-context)
+  (val (:pointer (:struct sqlite3-value))))
+
+(defcfun sqlite3-result-zeroblob :void
+  (ctx p-sqlite3-context)
+  (len :int))
+
+(defcfun sqlite3-value-blob :pointer
+  (val (:pointer (:struct sqlite3-value))))
+
+(defcfun sqlite3-value-bytes :int
+  (val (:pointer (:struct sqlite3-value))))
+
+(defcfun sqlite3-value-bytes16 :int
+  (val (:pointer (:struct sqlite3-value))))
+
+(defcfun sqlite3-value-double :double
+  (val (:pointer (:struct sqlite3-value))))
+
+(defcfun sqlite3-value-int :int
+  (val (:pointer (:struct sqlite3-value))))
+
+(defcfun sqlite3-value-int64 :int64
+  (val (:pointer (:struct sqlite3-value))))
+
+(defcfun sqlite3-value-text :string
+  (val (:pointer (:struct sqlite3-value))))
+
+(defcfun sqlite3-value-text16 :pointer
+  (val (:pointer (:struct sqlite3-value))))
+
+(defcfun sqlite3-value-text16le :pointer
+  (val (:pointer (:struct sqlite3-value))))
+
+(defcfun sqlite3-value-text16be :pointer
+  (val (:pointer (:struct sqlite3-value))))
+
+;;The sqlite3_value_type() family of interfaces require protected
+;;sqlite3_value objects.
+
+(defcfun sqlite3-value-type :int
+  (val (:pointer (:struct sqlite3-value))))
+
+(defcfun sqlite3-value-numeric-type :int
+  (val (:pointer (:struct sqlite3-value))))
+
+;;
+
+(defcfun sqlite3-create-function sqlite-ffi::error-code
+  (db p-sqlite3)
+  (fname :string)
+  (nargs :int)
+  (extetrep :int)			; text encoding SQLITE_ANY
+  (papp (:pointer :void))		; user data
+  (xfunc (:pointer :void))
+  (xstep (:pointer :void))
+  (xfinal (:pointer :void))
+  )
+
+;;  void (*xFunc)(sqlite3_context*,int,sqlite3_value**),
+;;  void (*xStep)(sqlite3_context*,int,sqlite3_value**),
+;;  void (*xFinal)(sqlite3_context*)
+
+;; EXAMPLE:
+#+nil
+(cffi:defcallback log_e :void
+    ((ctx (:pointer (:struct sqlite3-context)))
+     (n :int)
+     (arg (:pointer (:pointer (:struct sqlite3-value)))))
+  (let* ((ptr (cffi:mem-ref arg '(:pointer (:struct sqlite3-value))))
+	 (par (sqlite3-value-double ptr))
+	 (ret (cffi:foreign-funcall "log" :double par :double)))
+    (format t "log_e ~A => ~A ignoring n=~A" par ret n)
+    (sqlite3-result-double ctx ret)))
+
+#+nil
+(sqlite-ffi::sqlite3-create-function (sqlite::handle *db*)
+				     "log_e"  ;name
+				     1	      ;nargs
+				     1	      ;text encoding
+				     (cffi:null-pointer) ;user data
+				     (cffi:callback log_e) ;xfunc
+				     (cffi:null-pointer)   ;xstep
+				     (cffi:null-pointer))  ;xfinal
+
+#+nil
+(sqlite:execute-to-list *db* "SELECT log_e(100)")
